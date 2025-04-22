20479c44290d51deaa5f7704cde188c6405b1732
Sarathmanda <sarathmanda99@gmail.com>
2025-04-22 16:15:25 +0530

    Done

--- src/main/java/StringReverse.java
+++ src/main/java/StringReverse.java
@@ -13,6 +13,7 @@ public class StringReverse {
      * @return the reverse of str.
      */
     public String reverse(String str){
-        return null;
+        return new
+        StringBuilder(str).reverse().toString();
     }
 }
